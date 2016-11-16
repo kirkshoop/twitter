11397fe95abc342a0de3937dd51e6ec40ca1813e
Kirk Shoop <kirk.shoop@microsoft.com>
2016-11-15 23:13:35 -0800

    improve word list

--- main.cpp
+++ main.cpp
@@ -69,10 +69,11 @@ struct WordCount
     vector<float> all;
 };
 
-set<string> ignoredWords{
+const unordered_set<string> ignoredWords{
 // added
 "rt", "like", "just", "tomorrow", "new", "year", "month", "day", "today", "make", "let", "want", "did", "going", "good", "really", "know", "people", "got", "life", "need", "say", "doing", "great", "right", "time", "best", "happy", "stop", "think", "world", "watch", "gonna", "remember", "way",
-"better", "team", "check", "feel", "talk", "hurry", "look", "live", "home", "game", "run",
+"better", "team", "check", "feel", "talk", "hurry", "look", "live", "home", "game", "run", "im", "dont", "youre", "person", "house", "real", "thing", "lol", "has", "things", "thats", "fine", "ive", "youve", "yall", "didnt", "said", "come", "coming", "havent", "wont", "cant", "dont", "shouldnt",
+"hasnt", "id", "youll", "e280a6",
 // http://xpo6.com/list-of-english-stop-words/
 "a", "about", "above", "above", "across", "after", "afterwards", "again", "against", "all", "almost", "alone", "along", "already", "also","although","always","am","among", "amongst", "amoungst", "amount",  "an", "and", "another", "any","anyhow","anyone","anything","anyway", "anywhere", "are", "around", "as",  "at", "back","be","became", "because","become","becomes", "becoming", "been", "before", "beforehand", "behind", "being", "below", "beside", "besides", "between", "beyond", "bill", "both", "bottom","but", "by", "call", "can", "cannot", "cant", "co", "con", "could", "couldnt", "cry", "de", "describe", "detail", "do", "done", "down", "due", "during", "each", "eg", "eight", "either", "eleven","else", "elsewhere", "empty", "enough", "etc", "even", "ever", "every", "everyone", "everything", "everywhere", "except", "few", "fifteen", "fify", "fill", "find", "fire", "first", "five", "for", "former", "formerly", "forty", "found", "four", "from", "front", "full", "further", "get", "give", "go", "had", "has", "hasnt", "have", "he", "hence", "her", "here", "hereafter", "hereby", "herein", "hereupon", "hers", "herself", "him", "himself", "his", "how", "however", "hundred", "ie", "if", "in", "inc", "indeed", "interest", "into", "is", "it", "its", "itself", "keep", "last", "latter", "latterly", "least", "less", "ltd", "made", "many", "may", "me", "meanwhile", "might", "mill", "mine", "more", "moreover", "most", "mostly", "move", "much", "must", "my", "myself", "name", "namely", "neither", "never", "nevertheless", "next", "nine", "no", "nobody", "none", "noone", "nor", "not", "nothing", "now", "nowhere", "of", "off", "often", "on", "once", "one", "only", "onto", "or", "other", "others", "otherwise", "our", "ours", "ourselves", "out", "over", "own","part", "per", "perhaps", "please", "put", "rather", "re", "same", "see", "seem", "seemed", "seeming", "seems", "serious", "several", "she", "should", "show", "side", "since", "sincere", "six", "sixty", "so", "some", "somehow", "someone", "something", "sometime", "sometimes", "somewhere", "still", "such", "system", "take", "ten", "than", "that", "the", "their", "them", "themselves", "then", "thence", "there", "thereafter", "thereby", "therefore", "therein", "thereupon", "these", "they", "thickv", "thin", "third", "this", "those", "though", "three", "through", "throughout", "thru", "thus", "to", "together", "too", "top", "toward", "towards", "twelve", "twenty", "two", "un", "under", "until", "up", "upon", "us", "very", "via", "was", "we", "well", "were", "what", "whatever", "when", "whence", "whenever", "where", "whereafter", "whereas", "whereby", "wherein", "whereupon", "wherever", "whether", "which", "while", "whither", "who", "whoever", "whole", "whom", "whose", "why", "will", "with", "within", "without", "would", "yet", "you", "your", "yours", "yourself", "yourselves", "the"};
 
@@ -290,9 +291,21 @@ int main(int argc, const char *argv[])
                     }
 
                     // exclude entities, urls and some punct from this words list
-                    auto words = split(tweettext(tweet), R"(((&\w;)|((http|ftp|https)://[\w-]+(\.[\w-]+)+([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?)|[\(\)\[\]\s,.-:;&?!'"]+))", Split::RemoveDelimiter); //"
+                    auto text = tweettext(tweet);
+                    regex ignore(R"((&[\w]+;)|((http|ftp|https)://[\w-]+(\.[\w-]+)+([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?)|['\""])"); //"
+                    auto words = split(regex_replace(text, ignore, ""), R"([\(\)\[\]\s,.-:;&?!]+)", Split::RemoveDelimiter);
                     for (auto& word: words) {
-                        word = tolower(word);
+                        auto w = tolower(word);
+                        word.clear();
+                        for(auto& c : w){
+                            if(c < 0) {
+                                stringstream hexed;
+                                hexed << hex << nouppercase << setfill('0') << setw(2) << static_cast<unsigned int>(static_cast<unsigned char>(c));
+                                word += hexed.str();
+                            } else {
+                                word.push_back(c);
+                            }
+                        }
                     }
                     words.erase(std::remove_if(words.begin(), words.end(), [=](const string& w){
                         return !(w.size() > 1 && ignoredWords.find(w) == ignoredWords.end() && URL.find(w) == string::npos);
@@ -494,8 +507,8 @@ int main(int argc, const char *argv[])
 
                         time_t tb = duration_cast<seconds>(window.begin).count();
                         struct tm* tmb = gmtime(&tb);
-                        std::stringstream buffer;
-                        buffer << std::put_time(tmb, "%a %b %d %H:%M:%S %Y");
+                        stringstream buffer;
+                        buffer << put_time(tmb, "%a %b %d %H:%M:%S %Y");
 
                         ImGui::Text("Start : %lld, %s", window.begin.count(), buffer.str().c_str());
                         ImGui::Text("Tweets: %ld", group->tweets.size());
